803b66d94cd419ec64cc077d8dcc4d9e1b294217
Michael Heuer <michael.heuer@rwth-aachen.de>
2024-02-28 09:28:57 +0100

    doc: improve plugin settings comments

--- packages/contracts/plugin-settings.ts
+++ packages/contracts/plugin-settings.ts
@@ -4,14 +4,13 @@ import {VersionTag} from '@aragon/osx-commons-sdk';
 
 export const PLUGIN_CONTRACT_NAME = 'Admin';
 export const PLUGIN_SETUP_CONTRACT_NAME = 'AdminSetup';
-export const PLUGIN_REPO_ENS_SUBDOMAIN_NAME = 'admin'; // This will result in the ENS domain name 'my.plugin.dao.eth'
+export const PLUGIN_REPO_ENS_SUBDOMAIN_NAME = 'admin'; // 'admin.plugin.dao.eth'
 
 export const VERSION: VersionTag = {
   release: 1, // Increment this number ONLY if breaking/incompatible changes were made. Updates between releases are NOT possible.
   build: 2, // Increment this number if non-breaking/compatible changes were made. Updates to newer builds are possible.
 };
 
-/* DO NOT CHANGE UNLESS YOU KNOW WHAT YOU ARE DOING */
 export const METADATA = {
   build: buildMetadata,
   release: releaseMetadata,
